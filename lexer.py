09861d451e38980070abf16e2fd4fe53d856781f
Kenneth Wilkins <kw76922@gmail.com>
2026-02-13 11:12:34 -0800

    Additional expressions added.

--- lexer.py
+++ lexer.py
@@ -1,2 +1,4 @@
 keywords_expressions = [r"if", r"else", r"int", r"float"]
 operators_expressions = [r"\=", r"\+", r"\>", r"*"]
+separators_expressions = [r"\(", r"\)", r"\{", r"\}", r"\;", r"\,"]
+identifiers_expressions = [r"[a-zA-Z_][a-zA-Z0-9_]*"]

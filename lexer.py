a62eddd3403e2c8c69920810a7947456e835f651
Amanda <chaab003@csusm.edu>
2026-02-23 11:50:10 -0800

    deleted comments

--- lexer.py
+++ lexer.py
@@ -1,10 +1,5 @@
 import re
 
-#keywords_expressions = [r"if", r"else", r"int", r"float"]
-#operators_expressions = [r"\=", r"\+", r"\>", r"*"]
-#separators_expressions = [r"\(", r"\)", r"\{", r"\}", r"\;", r"\,"]
-#identifiers_expressions = [r"[a-zA-Z_][a-zA-Z0-9_]*"]
-
 def CutOneLineTokens(program_code):
     """
     Tokenize one line of TinyPie code
